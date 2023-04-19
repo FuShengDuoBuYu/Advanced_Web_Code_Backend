85adc8b950a99548772e53bfbbb8fb9369836ef2
Junan Zhang <annnmusic37@gmail.com>
2023-04-20 00:45:17 +0800

    init swagger

--- src/main/java/com/se/advancedweb/controller/UserController.java
+++ src/main/java/com/se/advancedweb/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.se.advancedweb.controller;
 
 import com.se.advancedweb.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "用户管理")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -16,6 +19,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiOperation(value = "获取所有用户")
     @GetMapping("/all")
     public String helloUser() {
         return userService.getAllUser().toString();
